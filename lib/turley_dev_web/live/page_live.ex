bcbee33c3a7ef84079caa05dfd163926be1a261e
Stephen Turley <stephen@turley.dev>
2021-06-08 08:53:05 -0400

    can type in an input element

--- lib/turley_dev_web/live/page_live.ex
+++ lib/turley_dev_web/live/page_live.ex
@@ -3,37 +3,24 @@ defmodule TurleyDevWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{})}
+    {:ok, assign(socket, typing: false, msg: "")}
   end
 
   @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
+  def handle_event("send_message", %{"msg" => msg}, socket) do
+    IO.inspect(socket)
+    {:noreply, assign(socket, msg: msg)}
   end
 
   @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
-
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
-    end
+  def handle_event("typing", %{"msg" => msg}, socket) do
+    IO.inspect(socket)
+    {:noreply, assign(socket, typing: true, msg: msg)}
   end
 
-  defp search(query) do
-    if not TurleyDevWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
-
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
+  @impl true
+  def handle_event("stopped_typing", _, socket) do
+    IO.inspect(socket)
+    {:noreply, assign(socket, typing: false)}
   end
 end
