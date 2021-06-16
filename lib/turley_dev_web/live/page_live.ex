d319752e188682b2edc59adf2a53fc67bc2ad9e4
Stephen Turley <stephen@turley.dev>
2021-06-16 12:15:43 -0400

    can now create posts

--- lib/turley_dev_web/live/page_live.ex
+++ lib/turley_dev_web/live/page_live.ex
@@ -1,26 +1,21 @@
 defmodule TurleyDevWeb.PageLive do
   use TurleyDevWeb, :live_view
+  alias TurleyDev.Repo
+  alias TurleyDev.Post
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, typing: false, msg: "")}
+    posts = Repo.all(Post)
+    {:ok, assign(socket, typing: false, msg: "", posts: posts)}
   end
 
   @impl true
   def handle_event("send_message", %{"msg" => msg}, socket) do
     IO.inspect(socket)
-    {:noreply, assign(socket, msg: msg)}
-  end
 
-  @impl true
-  def handle_event("typing", %{"msg" => msg}, socket) do
-    IO.inspect(socket)
-    {:noreply, assign(socket, typing: true, msg: msg)}
-  end
+    Post.changeset(%Post{}, %{content: msg})
+    |> Repo.insert
 
-  @impl true
-  def handle_event("stopped_typing", _, socket) do
-    IO.inspect(socket)
-    {:noreply, assign(socket, typing: false)}
+    {:noreply, assign(socket, msg: "", posts: Repo.all(Post))}
   end
 end
