de9b1d201502f5b3b3a66d2a66142e39778592f6
Stephen Turley <stephen@turley.dev>
2021-06-21 15:46:00 -0400

    all posts should get deleted when this is deployed

--- priv/repo/migrations/20210621180705_add_creator_to_posts.exs
+++ priv/repo/migrations/20210621180705_add_creator_to_posts.exs
@@ -1,7 +1,11 @@
 defmodule TurleyDev.Repo.Migrations.AddCreatorToPosts do
   use Ecto.Migration
+  alias TurleyDev.Repo
+  alias TurleyDev.Timeline.Post
 
   def change do
+    Repo.delete_all(Post)
+
     alter table("posts") do
       add(:creator_id, references(:users))
     end
